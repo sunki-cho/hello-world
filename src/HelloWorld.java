25111010f762462a2ebec946f0d8b9fcf8462c23
s <s@59.26.167.119>
2021-03-03 09:10:43 +0900

    Hello world 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
      public static void main(String[] orgs) {
           System.out.println("Hi Java");
           System.out.println("Ang gimmoddi");
+          System.out.println("Debt king");
           
        }
 }
