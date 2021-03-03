dd4c49890d618b4034abe4a1b4761fb7c9bbf967
s <s@59.26.167.119>
2021-03-03 09:03:11 +0900

    commit message

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+     public static void main(String[] orgs) {
+          System.out.println("Hi Java");
+          System.out.println("Ang gimmoddi");
+          
+       }
+}
+ 
